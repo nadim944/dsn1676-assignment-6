e9af2add1273cb3605c0c6b089ae51765777c6f7
nadim944 <nadeem944@hotmail.com>
2015-03-11 22:14:02 -0400

    Finished fifth section
    Added key frames and classes in css and main.js for list animation.
    Added webkits to animations and transforms

--- js/main.js
+++ js/main.js
@@ -3,25 +3,36 @@ var $box = $('.box');
 
 $showHide.on('click', function () {
     $box.toggleClass('js-box');
-})
+});
 
 var $move = $('.btn-move');
 var $diamond = $('.diamond');
 
 $move.on('click', function () {
     $diamond.toggleClass('move');
-})
+});
 
 var $collapse = $('.btn-collapse-expand');
 var $panel = $('.panel');
 
 $collapse.on('click', function () {
     $panel.toggleClass('collapse');
-})
+});
 
 var $bounce = $('.btn-bounce');
 var $circle = $('.circle');
 
 $bounce.on('click', function () {
     $circle.toggleClass('bounce');
-})
+});
+
+var $btnAppend = $('.btn-append');
+var $list = $('.list');
+
+$btnAppend.on('click', function () {
+    var $li = $('<li>').html('new list item');
+     
+    $list.prepend($li);
+    $li.toggleClass('list-animation');
+
+});
